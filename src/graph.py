4579c2571e45f9ed2cc6d74d1f0fa0ef45e42451
rgrosa <renanmathias99@gmail.com>
2024-04-22 11:24:45 -0300

    fix: format

--- src/graph.py
+++ src/graph.py
@@ -3,53 +3,35 @@ class Graph:
     def __init__(self, size, vertices, edges):
         self.graph = {}
 
-        #define the graph size
+        # define the graph size
         for i in range(int(size)):
             self.graph[i] = []
 
-
+        # populate vertices
         for vertex in vertices:
             id = int(vertex.get('@id'))
-            object = {'id': id, 'latitude': vertex.get('lat'), 'longitude':vertex.get('lon')}
+            object = {'id': id, 'latitude': vertex.get('lat'), 'longitude': vertex.get('lon')}
             self.graph[id].append({'vertex': object})
 
+        # populate edges
         for edge in edges:
-            vertexIn = int(edge.get('nodein'))
-            vertexOut = int(edge.get('nodeout'))
-            weigth =  float(edge.get('weigth'))
-            self.graph[vertexIn].append({'edge': {'vertexOut': vertexOut, 'weigth': weigth}})
-
+            vertex_in = int(edge.get('nodein'))
+            vertex_out = int(edge.get('nodeout'))
+            weight = float(edge.get('weight'))
+            self.graph[vertex_in].append({'edge': {'vertex_out': vertex_out, 'weight': weight}})
 
         for vertex, neighbors in self.graph.items():
             print(f"{vertex} -> {' '.join(map(str, neighbors))}")
 
-
-    '''
-    def __init__(self, V, edges):
-        self.adjacency_list = {}
-
-        # Add vertices to the dictionary
-        for i in range(V):
-            self.adjacency_list[i] = []
-
-        # Add edges to the dictionary
-        for edge in edges:
-            vertex1, vertex2 = edge
-            self.adjacency_list[vertex1].append(vertex2)
-
-        # Display the adjacency list
-        for vertex, neighbors in self.adjacency_list.items():
-            print(f"{vertex} -> {' '.join(map(str, neighbors))}")
- '''
     def get_neighbors(self, vertex_id):
         try:
             neighbors = []
             vertex_info = self.graph[vertex_id]
 
             for vertex in vertex_info:
-                if vertex.get('vertex') == None:
+                if vertex.get('vertex') is None:
                     edge = vertex.get('edge')
-                    neighbors.append(self.graph[edge.get('vertexOut')])
+                    neighbors.append(self.graph[edge.get('vertex_out')])
             return neighbors
         except KeyError:
             return None
--- src/main.py
+++ src/main.py
@@ -1,18 +1,64 @@
 from src.graph import Graph
-from src.search import Search
 import xmltodict
 
-file = xmltodict.parse(open("graph_test_tree.xml", "r").read())
-file_content = file.__getitem__('graph')
-graph_size = file_content.__getitem__('@size')
-nodes = file_content.__getitem__('nodes').__getitem__('node')
-edges = file_content.__getitem__('edges').__getitem__('edge')
 
-#V1 = 3
-#edges1 = [[0, 1], [1, 2], [2, 0]]
-#graph = Graph(3, edges1)
-graph = Graph(graph_size, nodes, edges)
-graph.get_neighbors(1)
-search = Search()
-dfs_result = search.dfs(graph, 0, 5)
-print(dfs_result)
+def dfs(graph, start, goal) -> (int, float, [int]):
+    """Busca em graph, um caminho entre start e goal usando busca em profundidade."""
+    stack = []
+    visited = []
+    stack.append(start)
+    number_of_visited = 0
+    length = 0.0
+    way = []
+    while stack:
+        vertex_id = stack.pop()
+        if goal == vertex_id:
+            return {"visited": number_of_visited, "length": length, "way": way}
+        if vertex_id not in visited:
+            number_of_visited, length, way = process(number_of_visited, length, way, vertex_id)
+            visited.append(vertex_id)
+            for u in graph.get_neighbors(vertex_id):
+                stack.append(graph.get_vertex_id(u))
+
+
+def bfs(graph, start: int, goal: int) -> (int, float, [int]):
+    """Busca em graph, um caminho entre start e goal usando busca em largura."""
+
+
+def branch_and_bound(graph, start: int, goal: int) -> (int, float, [int]):
+    """Busca em graph, um caminho entre start e goal usando Branch and Bound."""
+
+
+def a_star(graph, start: int, goal: int) -> (int, float, [int]):
+    """Busca em graph, um caminho entre start e goal usando A*."""
+
+
+def dijkstra(graph, start: int, goal: int) -> (int, float, [int]):
+    """Busca em graph, um caminho entre start e goal usando Dijkstra."""
+
+
+def process(visit, length, way, vertex_id) -> (int, float, [int]):
+    visit = visit + 1
+    length = length  ##todo ver oq é comprimento
+    way.append(vertex_id)
+    return visit, length, way
+
+
+def main():
+    graph = read_graph("graph_test_tree.xml")  ##coloca o nome do arquivo aqui
+    dfs_result = dfs(graph, 0, 5)
+    print(dfs_result)
+
+
+def read_graph(filename) -> Graph:
+    """Le a estrutura do grafo a partir de um arquivo."""
+    file = xmltodict.parse(open(filename, "r").read())
+    file_content = file.__getitem__('graph')
+    graph_size = file_content.__getitem__('@size')
+    nodes = file_content.__getitem__('nodes').__getitem__('node')
+    edges = file_content.__getitem__('edges').__getitem__('edge')
+    return Graph(graph_size, nodes, edges)
+
+
+if __name__ == '__main__':
+    main()
